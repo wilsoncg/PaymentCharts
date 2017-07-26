55c51fed1857b603857ea9211ccd40021c246bfb
Craig Wilson <craig@wilsoncg.net>
2017-07-26 18:56:44 +0100

    start grouping

--- Process/Library1.fs
+++ Process/Library1.fs
@@ -1,4 +1,4 @@
-﻿module Process
+﻿module Test
 
 open System
 open System.Data
@@ -6,8 +6,29 @@ open System.Data.Linq
 open FSharp.Data.TypeProviders
 open Microsoft.FSharp.Linq
 
-type Class1() = 
-    member this.X = "F#"
+type internal DB = SqlDataConnection<"Data Source=(localdb)\MSSqlLocalDB;Initial Catalog=PaymentsData;Integrated Security=SSPI;", Pluralize=true>
+ let private dc = DB.GetDataContext()
 
-type internal DB = SqlDataConnection<"Data Source=(localdb)\MSSqlLocalDB;Initial Catalog=PaymentsData;Integrated Security=SSPI;">
- let private lt = DB.GetDataContext().LedgerTransaction
+ let private sum = 
+  query {
+   for transaction in dc.LedgerTransactions do
+   where (transaction.LedgerTransactionDateTime > DateTime.UtcNow.Subtract(TimeSpan.FromDays(7.00)))
+   select transaction
+  } 
+  |> Seq.groupBy (fun tran -> tran.LedgerTransactionDateTime.Day)
+  |> Seq.map (fun tranDay -> 
+                            let day = fst tranDay
+                            let sumAmount = snd tranDay |> Seq.fold (fun acc tran -> acc + tran.Amount) 0m
+                            day, sumAmount)
+  
+  //|> Seq.fold (fun acc tran -> acc + tran.Amount) 0m
+  //|> Seq.iter (fun t -> t.Amount)
+  //|> Seq.fold (fun acc elem -> acc + elem) 0 transaction.A
+
+// line plot
+// x-axis, date (days)
+// y-axis, amount (sum), payment method (transaction type)
+
+// let x  = [1.; 2.; 3.; 4.; 5.; 6.; 7.; 8.; 9.; 10.; ]
+// let y = amount summed per day
+// yCardDeposit, yCardWithdrawal, yBank, etc
