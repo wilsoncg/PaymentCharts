a520ee527372ed85f9cd7f0220b9f45028266bbf
Craig Wilson <craig@wilsoncg.net>
2017-08-02 21:32:30 +0100

    group by

--- Process/Library1.fs
+++ Process/Library1.fs
@@ -4,23 +4,48 @@ open System
 open System.Data
 open System.Data.Linq
 open FSharp.Data.TypeProviders
+open FSharp.Plotly
 open Microsoft.FSharp.Linq
 
 type internal DB = SqlDataConnection<"Data Source=(localdb)\MSSqlLocalDB;Initial Catalog=PaymentsData;Integrated Security=SSPI;", Pluralize=true>
  let private dc = DB.GetDataContext()
 
- let private sum = 
+ let printSeq seq1 = Seq.iter (printf "%A ") seq1; printfn ""
+
+ let private d, t, s = 
   query {
    for transaction in dc.LedgerTransactions do
    where (transaction.LedgerTransactionDateTime > DateTime.UtcNow.Subtract(TimeSpan.FromDays(7.00)))
    select transaction
   } 
-  |> Seq.groupBy (fun tran -> tran.LedgerTransactionDateTime.Day)
-  |> Seq.map (fun tranDay -> 
-                            let day = fst tranDay
-                            let sumAmount = snd tranDay |> Seq.fold (fun acc tran -> acc + tran.Amount) 0m
-                            day, sumAmount)
+  |> Seq.groupBy (fun tran -> tran.LedgerTransactionDateTime.Day, tran.LedgerTransactionTypeId)
+  |> Seq.map (fun groupedTransactions -> 
+                            let day = 
+                             match fst groupedTransactions with
+                             | (tday, ttype) -> tday
+                            let ttype =
+                             match fst groupedTransactions with
+                             | (tday, ttype) -> ttype
+                            //let sequence = snd tranDay
+                            let sumAmount = snd groupedTransactions |> Seq.fold (fun acc tran -> acc + tran.Amount) 0m
+                            let depOrWith = snd groupedTransactions |> Seq.groupBy (fun tran -> tran.LedgerTransactionTypeId)
+                            day, ttype, sumAmount)
+  |> List.ofSeq 
+  |> List.unzip3 
   
+ //let days = d |> Seq.ofList
+
+ let chart = 
+  [
+   Chart.StackedBar(d,s,Name="dep");
+  //Chart.StackedBar(d,s,Name="with")
+  ]
+  |> Chart.Combine
+
+// [1, 2, 3, 4, 5]
+// [100, 50, 80, 110, 120]
+  
+//printSeq sum
   //|> Seq.fold (fun acc tran -> acc + tran.Amount) 0m
   //|> Seq.iter (fun t -> t.Amount)
   //|> Seq.fold (fun acc elem -> acc + elem) 0 transaction.A
